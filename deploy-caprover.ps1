bba439da766f9e4d8dd9ba6e99474f471f73a58b
rsanchez <rsanchez@sanmartin.com.ni>
2026-02-23 16:49:15 -0600

    ultimos cambios

--- deploy-caprover.ps1
+++ deploy-caprover.ps1
@@ -1,6 +1,9 @@
 # Build y deploy para CapRover (método build local + tar)
 # Uso: .\deploy-caprover.ps1
 
+$ErrorActionPreference = "Stop"
 npm run build
+if (-not (Test-Path "dist")) { Write-Error "No existe la carpeta dist. Revisa que npm run build haya terminado bien." }
 tar -cvf deploy.tar --exclude='*.map' captain-definition dist
-caprover deploy -t ./deploy.tar
+if (-not (Test-Path "deploy.tar")) { Write-Error "No se creó deploy.tar. Revisa el comando tar." }
+caprover deploy -t (Resolve-Path "deploy.tar").Path
